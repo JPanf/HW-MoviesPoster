947ea708f710a28a9852ee1514db1efb3331cc1f
Julia <julia.panfilova@mail.ru>
2022-08-20 15:41:01 +0300

    Initial commit

---
+++ src/main/java/ru/netology/MoviesPoster/services/MoviesPosterManager.java
@@ -0,0 +1,47 @@
+package ru.netology.MoviesPoster.services;
+
+public class MoviesPosterManager {
+    private String movies;
+    private String[] moviesPoster = new String[0];
+    private int limit;
+
+    public MoviesPosterManager(int limit){
+        this.limit=limit;
+    }
+    public MoviesPosterManager(){
+        this.limit=10;
+    }
+
+    public String[] addNewMovie (String movie) {
+        String[] newMovies = new String[moviesPoster.length + 1];
+        for (int i = 0; i < moviesPoster.length; i++) {
+            newMovies[i] = moviesPoster[i];
+        }
+        newMovies[newMovies.length - 1] = movie;
+        moviesPoster = newMovies;
+        return newMovies;
+    }
+
+    public String[] showMoviesLastToFirst() {
+        String[] allMovies = new String[limit];
+        for (int i = 0; i < allMovies.length; i++) {
+            allMovies[i] = moviesPoster[moviesPoster.length-1 - i];
+        }
+        return allMovies;
+    }
+
+    public String[] showAskedNumberOfLastMovies (int limit) {
+        int actualLimit= moviesPoster.length;
+        if (actualLimit >= limit) {
+            actualLimit = limit;
+        } else {
+            actualLimit = moviesPoster.length;
+        }
+        String[] askedNumberMovies = new String[actualLimit];
+        for (int i=0; i<askedNumberMovies.length; i++) {
+            askedNumberMovies[i] = moviesPoster[moviesPoster.length - 1 - i];
+        }
+        return askedNumberMovies;
+    }
+
+}
---
+++ src/test/java/ru/netology/MoviesPoster/services/MoviesPosterManagerTest.java
@@ -0,0 +1,75 @@
+package ru.netology.MoviesPoster.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.MoviesPoster.services.MoviesPosterManager;
+
+public class MoviesPosterManagerTest {
+    MoviesPosterManager manager = new MoviesPosterManager();
+
+    @Test
+    public void shouldAddNewMovie(){
+
+        String[] actual = manager.addNewMovie("film5");
+        String[] expected = {"film5"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowMoviesLastToFirst (){
+        manager.addNewMovie("film1");
+        manager.addNewMovie("film2");
+        manager.addNewMovie("film3");
+        manager.addNewMovie("film4");
+        manager.addNewMovie("film5");
+        manager.addNewMovie("film6");
+        manager.addNewMovie("film7");
+        manager.addNewMovie("film8");
+        manager.addNewMovie("film9");
+        manager.addNewMovie("film10");
+        manager.addNewMovie("film11");
+        manager.addNewMovie("film12");
+        manager.addNewMovie("film13");
+
+        String []actual = manager.showMoviesLastToFirst();
+        String[] expected = {"film13","film12","film11", "film10","film9","film8","film7","film6","film5","film4"};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowAskedNumberOfLastAddedMovies(){
+        manager.addNewMovie("film1");
+        manager.addNewMovie("film2");
+        manager.addNewMovie("film3");
+        manager.addNewMovie("film4");
+        manager.addNewMovie("film5");
+        manager.addNewMovie("film6");
+        manager.addNewMovie("film7");
+        manager.addNewMovie("film8");
+        manager.addNewMovie("film9");
+        manager.addNewMovie("film10");
+        manager.addNewMovie("film11");
+        manager.addNewMovie("film12");
+        manager.addNewMovie("film13");
+
+        String[] actual = manager.showAskedNumberOfLastMovies(4);
+        String[] expected = {"film13", "film12","film11","film10"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowAllMoviesIfLessThanAsked(){
+        manager.addNewMovie("film1");
+        manager.addNewMovie("film2");
+        manager.addNewMovie("film3");
+
+        String[] actual = manager.showAskedNumberOfLastMovies(7);
+        String[] expected = {"film3", "film2","film1"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+}
